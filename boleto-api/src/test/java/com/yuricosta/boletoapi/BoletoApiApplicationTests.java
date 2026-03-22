c3c837d64592409dc3c831b673e5cc9066fc2edf
Yuri-Costa09 <yurircostawork@gmail.com>
2026-03-21 21:57:39 -0300

    feat: add boleto-api microsservice

---
+++ boleto-api/src/test/java/com/yuricosta/boletoapi/BoletoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuricosta.boletoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
